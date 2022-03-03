84176aab6d38730195636d1e948df8ec6a9260fe
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-03-03 22:54:03 +0100

    add

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -8,7 +8,7 @@ describe("Form test", () => {
       .should("have.value", "Nayab");
 
     cy.get('input[name="email"]')
-      .type("nayab1@gmail.com")
+      .type("nayab12@gmail.com")
       .should("have.value", "nayab1@gmail.com");
 
     cy.get("textarea")
